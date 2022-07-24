ec020ce8b149134de9448ac6815c26e16d2b4266
Hosam293 <hossamjamal6@gmail.com>
2022-07-24 19:44:22 +0200

    hacka

---
+++ lib/model/Static/HomePage/FirstSectionModel.dart
@@ -0,0 +1,12 @@
+class FirstSectionModel {
+  String? title;
+  String? body;
+
+  FirstSectionModel({this.title, this.body});
+
+  FirstSectionModel.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    body = json['body'];
+  }
+
+}
